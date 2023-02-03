5b3179b12cd8cdd98fce4ad3d35209263bc0c905
Charlyboy1993 <cjaramilloportilla@gmail.com>
2023-02-02 19:37:53 -0500

    Implement react-redux and its confi in the index.js file

--- client/src/index.js
+++ client/src/index.js
@@ -2,16 +2,27 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { Provider } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
+import cartReducer from "./state";
 import { ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 import { theme } from "./theme";
 
+const store = configureStore({
+  reducer: {
+    cart: cartReducer,
+  },
+});
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <App />
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
+    </Provider>
   </React.StrictMode>
 );
